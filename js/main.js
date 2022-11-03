fe22d75c3c6d1b615b11706c5ee2d8b5d311295f
Smaidziunas <sdamarcius@gmail.com>
2022-11-03 09:25:03 +0200

    masyvu uzduotis

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
-'use strict';
-console.log('main.js');
+"use strict";
+console.log("main.js");
+
+const numbers = [
+  1, 2, 3, 5.3, 8, -8, -5.5, -6, -16, -5, -9, 4.2, 5, 12, -6, 45,
+];
+
+// 1. atrinkti skaicius didesnius uz 3 => filter;
+
+// 2. grazinti nauja masyva su padvigubintom reiksmem => map;
+
+// 3. gauti neigiamu reiksmiu bendra suma => reduce;
+
+// 4. suzinoti ar yra skaicius 5 musu numbers masyve? => includes;
+
+// 5. konsoleje atspausdinti visas reiksmes su indeksais => forEach;
+
+// 6. .every
+
+// 7. .some
